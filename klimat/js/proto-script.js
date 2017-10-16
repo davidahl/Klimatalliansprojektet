59e0efa7c143df8c4662c0bd226b34bc50272182
Chorol <Chorol@users.noreply.github.com>
2017-10-16 22:26:19 +0200

    Small fixes and categories

---
+++ klimat/js/proto-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	/**$('#wrapper').css({
+		'position' : 'absolute',
+		'left' : '50%',
+        'top' : '50%',
+        'margin-left' : -$('#wrapper').outerWidth()/2,
+        'margin-top' : -$('#wrapper').outerHeight()/2
+	});**/
+
+});
