559f3b8d17e806ed54e7b3531fe7f7f870090d99
Carl <carl.rikner@hotmail.se>
2017-11-07 15:06:53 +0100

    Rapporterings script klart

--- klimat/js/proto-script.js
+++ klimat/js/proto-script.js
@@ -1,12 +1,14 @@
-var outlets=[];
-var components=["input","option","output"];
-var rows=[];
 $(document).ready(function(){
+    /*
+    För rapport.html
+    */
     $("#stattable > tr").each(function() {
+
         /*
         Tar fram outputboxen
         */
         var output=$(this).find('input.inputbox').eq(1);
+
         /*
         Hanterar avfokusering på inputboxen
         */
@@ -15,13 +17,6 @@ $(document).ready(function(){
             if(!isNaN(val)){
                 output.val(val.toFixed(2));
             }
-
         });
     });
 });
-
-function calculate(input, output){
-    var val=parseInt($(input).val());
-    val*=8.87;
-    $(output).val(val.toFixed(2));
-};
