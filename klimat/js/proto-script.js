2e6c36de25b6daf1da31313aa3440ede7fa462f8
Carl <carl.rikner@hotmail.se>
2017-11-07 14:14:29 +0100

    Nu funkar scriptet för rapport.html

--- klimat/js/proto-script.js
+++ klimat/js/proto-script.js
@@ -2,25 +2,22 @@ var outlets=[];
 var components=["input","option","output"];
 var rows=[];
 $(document).ready(function(){
-    $("#stattable > tr").each(function(index, el) {
-
-    });
-    /*$(".outlet").each(function(){
-        outlets.push($(this).attr('name'));
-    });
-    for (var i in outlet) {
-        $("."+outlet[i]+components[0]).addEventListener('keypress', function(e){
+    $("#stattable > tr").each(function() {
+        /*
+        Tar fram outputboxen
+        */
+        var output=$(this).find('input.inputbox').eq(1);
+        /*
+        Hanterar avfokusering på inputboxen
+        */
+        $(this).find('input.inputbox').eq(0).focusout(function(event) {
+            var val=parseInt($(this).val());
+            if(!isNaN(val)){
+                output.val(val.toFixed(2));
+            }
 
         });
-    }*/
-    /**$('#wrapper').css({
-		'position' : 'absolute',
-		'left' : '50%',
-        'top' : '50%',
-        'margin-left' : -$('#wrapper').outerWidth()/2,
-        'margin-top' : -$('#wrapper').outerHeight()/2
-	});**/
-
+    });
 });
 
 function calculate(input, output){
