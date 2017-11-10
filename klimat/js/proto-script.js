0cbd2a7f18b8b57ef2f4d0bdb7fbd02d91c37ae3
davidahl <david.ahl.axelsson@gmail.com>
2017-11-10 12:03:49 +0100

    Add files via upload

--- klimat/js/proto-script.js
+++ klimat/js/proto-script.js
@@ -19,4 +19,19 @@ $(document).ready(function(){
             }
         });
     });
+	$("#addrow").click(function(){
+		var table = document.getElementById("reportTable");
+		var row = table.insertRow(table.length);
+		var cell1 = row.insertCell(0);
+		var cell2 = row.insertCell(1);
+		var cell3 = row.insertCell(2);
+		var cell4 = row.insertCell(3);
+		cell1.innerHTML = "<input type='text' class='inputbox'/>";
+		cell2.innerHTML = "<input type='text' class='inputbox'/>";
+		cell3.innerHTML = "<input type='text' class='inputbox'/>";
+		cell4.innerHTML = "<input type='text' class='inputbox'/>";
+		
+	});
 });
+
+
