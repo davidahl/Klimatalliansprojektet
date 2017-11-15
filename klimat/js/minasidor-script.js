c5966bd5c3291e0dd6d50da3d4f162e254e6a5a2
Carl <carl.rikner@hotmail.se>
2017-11-15 12:55:59 +0100

    Separerat ut stilar i separata css:er

--- klimat/js/minasidor-script.js
+++ klimat/js/minasidor-script.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
     $("#change").submit(function(){
-        
+
     });
 });
+
+function validateForm() {
+    var newpass = document.forms["passwchange"]["newpass"].value;
+    var newPassconf = document.forms["passwchange"]["newpassconf"].value;
+    if(newpass != "" && newpassconf != ""){
+        if (newpass != newPassconf) {
+            document.getElementById("nomatch").innerHTML = "Passwords don't match";
+            return false;
+        }else{
+            alert("Password changed");
+        }
+    }
+    var num=6;
+    document.getElementById("nomatch").innerHTML = "Password must be longer than " + num + " characters.";
+    return false
+};
