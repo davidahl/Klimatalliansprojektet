97dda8873d18ced2dc93b81df82a969a3d8ac06c
AegirA <aegir.atlason@gmail.com>
2017-11-21 10:07:42 +0100

    översätt från engelska till svenska

--- klimat/js/minasidor-script.js
+++ klimat/js/minasidor-script.js
@@ -8,23 +8,24 @@ function validateForm() {
     
     var newPassconf = document.forms["passwchange"]["newpassconf"].value;
     var newpass = document.forms["passwchange"]["newpass"].value;
-    var num=6;
+    var numMin=6;
+	var numMax=20;
 
     if(newPassconf != "" &&  newpass!= ""){
         if (newpass != newPassconf) {
-            document.getElementById("nomatch").innerHTML = "Passwords don't match";
+            document.getElementById("nomatch").innerHTML = "Lösenorden matchar inte";
             consol(newpass + " " + newpassconf);
             return false;
         }
         if(newpass.length < 6 && newPassconf.length < 6){
-             document.getElementById("nomatch").innerHTML = "Password must be longer than " + num + " characters.";
+             document.getElementById("nomatch").innerHTML = "Lösenordet måste vara mellan " + numMin + " och " + numMax + " tecken långt.";
              return false;
         }
-        alert("Password change");
+        alert("Lösenordet har ändrats");
         return true;
 
     }else{
-        document.getElementById("nomatch").innerHTML = "Fill in both feilds";
+        document.getElementById("nomatch").innerHTML = "Fälten matchar inte";
         return false;
     }
             
